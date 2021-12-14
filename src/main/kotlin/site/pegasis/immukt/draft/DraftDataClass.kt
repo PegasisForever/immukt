d2f8f8e33c2ca64df151111542e397adec831ae7
pegasis <me@pegasis.site>
2021-12-13 22:34:52 -0500

    optimize DraftDataClass

--- src/main/kotlin/site/pegasis/immukt/draft/DraftDataClass.kt
+++ src/main/kotlin/site/pegasis/immukt/draft/DraftDataClass.kt
@@ -14,11 +14,10 @@ inline val <T : DataClass> T.draft: DraftDataClass<T>
     get() = DraftDataClass(this)
 
 class DraftDataClass<T : DataClass>(
-    val data: T,
-    private val path: List<String>,
-    private val changeCache: HashMap<List<String>, Any?>, // todo don't initialize before first use
+    private val data: T,
 ) : Producible<T> {
-    constructor(data: T) : this(data, emptyList(), hashMapOf())
+    private val changeCache: HashMap<String, Any?> = hashMapOf()
+    private var produceCache: T? = data
 
     init {
         assert(data::class.isData)
@@ -26,23 +25,16 @@ class DraftDataClass<T : DataClass>(
 
     // get normal value
     operator fun <V> get(key: KProperty1<T, V>): V {
-        val getPath = buildList { addAll(path); add(key.name) }
-        return if (changeCache.containsKey(getPath)) {
-            changeCache[getPath] as V
+        val cached = changeCache[key.name]
+        return if (cached != null) {
+            cached as V
         } else {
             key.get(data)
         }
     }
 
     // get data class
-    operator fun <V : DataClass> get(key: KProperty1<T, V>): DraftDataClass<V> {
-        val getPath = buildList { addAll(path); add(key.name) }
-        return if (changeCache.containsKey(getPath)) {
-            DraftDataClass(changeCache[getPath] as V, getPath, changeCache)
-        } else {
-            DraftDataClass(key.get(data), getPath, changeCache)
-        }
-    }
+    operator fun <V : DataClass> get(key: KProperty1<T, V>) = getImpl(key, ::DraftDataClass)
 
     // get data list
     operator fun <V : List<I>, I : DataClass> get(key: KProperty1<T, V>) = getImpl(key, DataDraftList.Companion::from)
@@ -57,53 +49,47 @@ class DraftDataClass<T : DataClass>(
     operator fun <V : Set<I>, I> get(key: KProperty1<T, V>) = getImpl(key, ValueDraftSet.Companion::from)
 
     // get data map
-    operator fun <V : Map<K, MV>, K, MV : DataClass> get(key: KProperty1<T, V>) = getImpl(key, DataDraftMap.Companion::from)
+    operator fun <V : Map<K, MV>, K, MV : DataClass> get(key: KProperty1<T, V>) =
+        getImpl(key, DataDraftMap.Companion::from)
 
     // get value map
     operator fun <V : Map<K, MV>, K, MV> get(key: KProperty1<T, V>) = getImpl(key, ValueDraftMap.Companion::from)
 
     private inline fun <V, reified R> getImpl(key: KProperty1<T, V>, constructor: (v: V) -> R): R {
-        val getPath = buildList { addAll(path); add(key.name) }
-        val cached = changeCache[getPath]
-        return when (cached) {
-            null -> constructor(key.get(data))  // not cached
-            is R -> cached                      // cached draft
-            else -> constructor(cached as V)    // cached value
-        }.also {
-            changeCache[getPath] = it
+        return when (val cached = changeCache[key.name]) {
+            null -> constructor(key.get(data)).also {  // not cached
+                produceCache = null
+                changeCache[key.name] = it
+            }
+            is R -> cached                             // cached draft
+            else -> constructor(cached as V).also {    // cached value
+                produceCache = null
+                changeCache[key.name] = it
+            }
         }
     }
 
     operator fun <V> set(key: KProperty1<T, V>, value: V) {
-        val setPath = buildList { addAll(path); add(key.name) }
-        changeCache[setPath] = value
+        produceCache = null
+        changeCache[key.name] = value
     }
 
-    // todo cache
     override fun produce(): T {
-        val originalPropertyMap = data::class.declaredMemberProperties.associate {
-            it.name to it.getter
-        }
-
-        fun getOriginalProperty(name: String): Any? {
-            return originalPropertyMap[name]!!.call(data)
-        }
-
-        val constructor = data::class.primaryConstructor!!
-        val params = constructor.parameters.map { param ->
-            val paramPath = buildList { addAll(path); add(param.name!!) }
-            val value = if (changeCache.containsKey(paramPath)) {
-                changeCache[paramPath]
-            } else {
-                getOriginalProperty(param.name!!)
-            }
-            when (value) {
-                is DataClass -> DraftDataClass(value, paramPath, changeCache).produce()
-                is Producible<*> -> value.produce()
-                else -> value
+        if (produceCache != null) return produceCache!!
+        val klass = data::class
+        val properties = klass.declaredMemberProperties.associate { it.name to it.getter }
+        val constructor = klass.primaryConstructor!!
+        val params = Array(constructor.parameters.size) { i ->
+            val param = constructor.parameters[i]
+            when (val cached = changeCache[param.name]) {
+                null -> properties[param.name]!!.call(data)
+                is Producible<*> -> cached.produce()
+                else -> cached
             }
         }
-        return constructor.call(*params.toTypedArray())
+
+        produceCache = constructor.call(*params)
+        return produceCache!!
     }
 
     override fun toString(): String {
